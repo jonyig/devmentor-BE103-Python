b3230196ff7b18aa2b195fb60bcdc41c2c454011
irene3221 <irene18883@gmail.com>
2024-02-09 02:21:13 +0800

    adding db method for delete/patch

--- repository/post.py
+++ repository/post.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 
 from database.post import Post
+from database.postupdate import PostUpdate
 from schema.database.post import PostCreate
 
 
@@ -14,3 +15,23 @@ def create(db: Session, post: PostCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def get_post(db: Session, post_id: int):
+    return db.query(Post).filter(Post.id == post_id).first()
+
+
+def delete(db: Session, post: Post):
+    db.delete(post)
+    db.commit()
+
+
+def patch_post(db: Session, post_id: int, post_update: PostUpdate):
+    db_post = db.query(Post).filter(Post.id == post_id).first()
+    if db_post:
+        for field, value in post_update.dict(exclude_unset=True).items():
+            setattr(db_post, field, value)
+        db.commit()
+        db.refresh(db_post)
+        return db_post
+    return None
