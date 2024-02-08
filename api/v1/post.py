b1e0393e0df7f8189a4a0ed2b6122da3902008b2
irene3221 <irene18883@gmail.com>
2024-02-09 02:18:47 +0800

    adding delete/patch/get router

--- api/v1/post.py
+++ api/v1/post.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 import repository.post
+from database.postupdate import PostUpdate
 from infrastructure.mysql import get_db
 from schema.database.post import PostCreate
 
@@ -11,7 +12,7 @@ router = APIRouter(
 )
 
 
-@router.get("/")
+@router.get("/list")
 def list_post(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     posts = repository.post.lists(db, skip=skip, limit=limit)
     return posts
@@ -20,3 +21,25 @@ def list_post(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 @router.post("/")
 def create_post(post: PostCreate, db: Session = Depends(get_db)):
     return repository.post.create(db=db, post=post)
+
+
+@router.delete("/{post_id}")
+def delete(post_id: int, db: Session = Depends(get_db)):
+    post = repository.post.get_post(db, post_id)
+    return repository.post.delete(db=db, post=post)
+
+
+@router.patch("/{post_id}")
+def update_post(post_id: int, post_update: PostUpdate, db: Session = Depends(get_db)):
+    existing_post = repository.post.get_post(db, post_id)
+    if existing_post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    updated_post = repository.post.patch_post(db, post_id, post_update)
+    return updated_post
+
+@router.get("/{post_id}")
+def get_post(post_id: int, db: Session = Depends(get_db)):
+    post = repository.post.get_post(db, post_id)
+    if post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return post
