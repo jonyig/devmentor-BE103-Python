59c57f22a1142f2797bdcbfc1e493d529e215b4f
irene3221 <irene18883@gmail.com>
2024-02-25 01:56:13 +0800

    use alembic to create "notify" table

--- myAlembic/versions/847d1fbc4d74_create_user_table.py
+++ myAlembic/versions/847d1fbc4d74_create_user_table.py
@@ -22,8 +22,7 @@ def upgrade() -> None:
     op.create_table(
         "notify",
         sa.Column("id", sa.Integer(), primary_key=True),
-        sa.Column("name", sa.String(length=255), nullable=False),
-    )
+        sa.Column("name", sa.String(length=255), nullable=False),)
     pass
 
 
