6645fc684b9c2ae60588ef56d3123b5bfc4f32e0
Jonny <ji394su311203106@gmail.com>
2024-02-25 22:35:12 +0800

    feat: add auth_router to route

--- api/general/routes.py
+++ api/general/routes.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
 from api.general.index import router as index_router
+from api.general.auth import router as auth_router
 
 routers = APIRouter()
 router_list = [
     index_router,
+    auth_router,
 ]
 
 for router in router_list:
