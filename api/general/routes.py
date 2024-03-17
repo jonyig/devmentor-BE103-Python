d69008ddb5ea6d4b19c08302998bb3a6f3ea07a1
irene3221 <irene18883@gmail.com>
2024-03-17 16:27:58 +0800

    adding router for auth with database

--- api/general/routes.py
+++ api/general/routes.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter
 
 from api.general.index import router as index_router
 from api.general.auth import router as auth_router
+from api.general.auth_db import router as auth_db_router
 
 routers = APIRouter()
 router_list = [
     index_router,
     auth_router,
+    auth_db_router
 ]
 
 for router in router_list:
