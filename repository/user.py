41cc2e4ac8ae1087d913e666d511eb2991667a2d
irene3221 <irene18883@gmail.com>
2024-03-21 20:39:34 +0800

    for 2 user classes with/without password need 2 function

--- repository/user.py
+++ repository/user.py
@@ -1,5 +1,8 @@
 from sqlalchemy.orm import Session
-from schema.database.user import User
+from schema.database.user import User,UserWithPassword
 
 def get_user(db: Session, username: str):
+    return db.query(UserWithPassword).filter(UserWithPassword.username == username).first()
+
+def get_user_data(db: Session, username: str):
     return db.query(User).filter(User.username == username).first()
